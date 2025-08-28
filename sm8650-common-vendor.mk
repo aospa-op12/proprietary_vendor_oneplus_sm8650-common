e82a0c5985815bfdfed0d0b955e84990830e7902
Sean Du <do4suki@gmail.com>
2025-08-28 19:02:01 +0800

    sm8650-common: Add oplusSensor AIDL
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -1059,6 +1059,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/android.hardware.contexthub-service.qmi.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.contexthub-service.qmi.rc \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/android.hardware.secure_element-service.qti.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.secure_element-service.qti.rc \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/init.touchDaemon.rc:$(TARGET_COPY_OUT_ODM)/etc/init/init.touchDaemon.rc \
+    vendor/oneplus/sm8650-common/proprietary/odm/etc/init/vendor-oplus-hardware-oplusSensor-V1-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor-oplus-hardware-oplusSensor-V1-service.rc \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/vendor-oplus-hardware-performance-V1-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor-oplus-hardware-performance-V1-service.rc \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/vendor-oplus-hardware-touch-V2-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor-oplus-hardware-touch-V2-service.rc \
     vendor/oneplus/sm8650-common/proprietary/odm/etc/init/vendor.oplus.hardware.biometrics.fingerprint@2.1-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.biometrics.fingerprint@2.1-service.rc \
@@ -3024,6 +3025,7 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.displaypanelfeature-V1-ndk \
     vendor.oplus.hardware.olc2-V1-ndk \
     vendor.oplus.hardware.olc2-V2-ndk \
+    vendor.oplus.hardware.oplusSensor-V1-ndk \
     vendor.oplus.hardware.performance-V1-ndk \
     vendor.oplus.hardware.performance-V2-ndk \
     vendor.oplus.hardware.performance-V3-ndk \
@@ -3218,6 +3220,7 @@ PRODUCT_PACKAGES += \
     snapseparateYUV2RGBBinary \
     android.hardware.contexthub-service.qmi.xml \
     manifest_displaycolorfeature_aidl.xml \
+    manifest_oplusSensor_aidl.xml \
     manifest_oplus_camera_rfi.xml \
     manifest_oplus_cammidasservice_aidl.xml \
     manifest_oplus_charger_aidl.xml \
@@ -3343,6 +3346,7 @@ PRODUCT_PACKAGES += \
     xtwifi-client \
     cwb_utils_aidl \
     android.hardware.secure_element-service.qti \
+    vendor-oplus-hardware-oplusSensor-V1-service \
     vendor-oplus-hardware-performance-V1-service \
     vendor-oplus-hardware-touch-V2-service \
     vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff \
